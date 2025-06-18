7f60794b93f064b367f38b8e7ccff8c7de93a2e2
MiguelCode <miguel.code@outlook.com>
2025-06-18 15:26:40 -0400

    añadir funcionalidad para enviar mensajes de WhatsApp con mensaje predeterminado segun el servicio desde el componente Inicio

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
 import {NabvarComponent} from '../nabvar/nabvar.component';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-inicio',
   imports: [
-    NabvarComponent
+    NabvarComponent,
+    FormsModule
   ],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
 export class InicioComponent {
+  numeroWhatsap: string = '';
+  servicioSeleccionado: string = 'ChatGPT Plus';
+
+  serviciosMensajes: { [key: string]: string } = {
+    'ChatGPT Plus': 'Hola, va renovar su suscripción a ChatGPT Plus.',
+    'Netflix': 'Hola, va renovar su suscripción a Netflix.',
+    'Spotify': 'Hola, va renovar su suscripción a Spotify.'
+  }; // Agregué punto y coma aquí
+
+  // Este método estaba faltando
+  enviarMensaje(): void {
+    const mensaje = this.serviciosMensajes[this.servicioSeleccionado];
+
+    const formatearNumero = `591${this.numeroWhatsap.replace(/\D/g, '')}`;
+
+    const whatsappURL = `https://wa.me/${formatearNumero}?text=${encodeURIComponent(mensaje)}`;
+
+    window.open(whatsappURL, '_blank');
+  }
+
 
 }
